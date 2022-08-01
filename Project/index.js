cece1fdddb44b45657056606c58fed9462dc0771
shitishpatel <68268691+shitishpatel@users.noreply.github.com>
2022-08-01 19:18:35 +0530

    Update index.js

--- Project/index.js
+++ Project/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const knex = require('./db/db');
+const jwt = require('jsonwebtoken');
 const ejs = require('ejs');
+const bcrypt = require('bcrypt');
 // const students = require('./student');
 // const getData= require('./server/get');
 const app =express();
 const student= require('./routes/student.js');
 const teacher= require('./routes/teacher.js');
+const passport = require('passport');
 // const app =express();
+const {initializingPassport} = require("./passportConfig.js");
+const expressSession = require('express-session');
+
+
+initializingPassport(passport);
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 // app.use('./server/get',getData);
 app.set("view engine","ejs");
 app.use('/student',student);
 app.use('/teacher',teacher);
+app.use(expressSession({secret:"secret",resave:false,saveUninitialized:false}))
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.get('/',(req,res)=>{
     res.render('index');

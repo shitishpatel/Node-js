6e4332ba1a779cc79cbf43fe3fe9d4ee14d8ab44
shitishpatel <68268691+shitishpatel@users.noreply.github.com>
2022-08-01 19:20:07 +0530

    Create passportConfig.js

---
+++ Project/passportConfig.js
@@ -0,0 +1,28 @@
+// const passport = require("passport");
+const LocalStrategy=require('passport-local').Strategy;
+const {student} = require("./db/db");
+exports.initializingPassport = (passport)=>{
+
+    passport.use(new LocalStrategy(
+        function(username, password, done) {
+          student.findOne({ username: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) { return done(null, false); }
+            if (user.passwordn !==password) { return done(null, false); }
+            return done(null, user);
+          });
+        }
+      ));
+
+
+      passport.serializeUser((user,done)=>done(null,user.id));
+
+      passport.deserializeUser(async (id,done)=>{
+        try{
+            const user= await student.findById(id);
+            done(null,user);
+        }catch(error){
+            done(error,false);
+        }
+      });
+};

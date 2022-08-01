8c63b65ef866605d64941eb706951dc6e9e15a18
shitishpatel <68268691+shitishpatel@users.noreply.github.com>
2022-08-01 19:17:29 +0530

    Update student.js

--- Project/routes/student.js
+++ Project/routes/student.js
@@ -1,9 +1,18 @@
 const express = require('express');
 // import express from 'express';
 const bcrypt = require('bcrypt');
-
+const passport = require('passport');
 const router =express.Router();
 const knex=require('../db/db');
+
+const {initializingPassport} = require("../passportConfig.js");
+const expressSession = require('express-session');
+initializingPassport(passport);
+
+router.use(expressSession({secret:"secret",resave:false,saveUninitialized:false}))
+router.use(passport.initialize());
+router.use(passport.session());
+
 
 router.post('/create',async(req,res)=>{
     try{
@@ -30,6 +39,39 @@ router.post('/create',async(req,res)=>{
     //                 res.send(student);
     //             });
     // });
+});
+
+
+router.post('/register',async(req,res)=>{
+    try{
+        const hashedPassword=await bcrypt.hash(req.body.password,10);
+        knex('student').insert({
+            // id:req.body.id,
+            first_name:req.body.first_name,
+            last_name:req.body.last_name,
+            email:req.body.email,
+            address:req.body.address,
+            password:hashedPassword,
+        }).then(()=>{
+            knex.select().from('student').then((student)=>{
+                res.send(student);
+            });
+        });
+
+    }catch(error){
+        res.status(500).json({error:error.message});
+    }
+
+    // knex.raw('insert into student (first_name,last_name) values(?,?)',['first','last'] ).then(()=>{
+    //     knex.select().from('student').then((student)=>{
+    //                 res.send(student);
+    //             });
+    // });
+});
+
+
+router.post('/login', passport.authenticate("local",{failureRedirect:"/register"}),async(req,res)=>{
+    res.send('student register');
 });
 
 // router.post('/api/students' ,(req,res)=>{

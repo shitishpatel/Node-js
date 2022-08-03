8e760c798dd2fa3152718239ca6e5ed2d30ff009
shitishpatel <68268691+shitishpatel@users.noreply.github.com>
2022-08-03 15:18:28 +0530

    Update student.js

--- Project/routes/student.js
+++ Project/routes/student.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// import express from 'express';
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 const router =express.Router();
@@ -34,11 +33,6 @@ router.post('/create',async(req,res)=>{
         res.status(500).json({error:error.message});
     }
 
-    // knex.raw('insert into student (first_name,last_name) values(?,?)',['first','last'] ).then(()=>{
-    //     knex.select().from('student').then((student)=>{
-    //                 res.send(student);
-    //             });
-    // });
 });
 
 
@@ -46,7 +40,6 @@ router.post('/register',async(req,res)=>{
     try{
         const hashedPassword=await bcrypt.hash(req.body.password,10);
         knex('student').insert({
-            // id:req.body.id,
             first_name:req.body.first_name,
             last_name:req.body.last_name,
             email:req.body.email,
@@ -62,49 +55,14 @@ router.post('/register',async(req,res)=>{
         res.status(500).json({error:error.message});
     }
 
-    // knex.raw('insert into student (first_name,last_name) values(?,?)',['first','last'] ).then(()=>{
-    //     knex.select().from('student').then((student)=>{
-    //                 res.send(student);
-    //             });
-    // });
 });
 
 
-router.post('/login', passport.authenticate("local",{failureRedirect:"/register"}),async(req,res)=>{
-    res.send('student register');
-});
+router.post('/login', passport.authenticate("local",{failureRedirect:"/student/register" ,successRedirect:"/"})
 
-// router.post('/api/students' ,(req,res)=>{
-//     console.log(req);
-//     res.send("post request");
-// })
+);
 
-// router.post('/register',async(req,res)=>{
-//     try{
-//         const hashedPassword=await bcrypt.hash(req.body.password,10);
-//         knex('student').insert({
-//             // id:req.body.id,
-//             first_name:req.body.first_name,
-//             last_name:req.body.last_name,
-//             email:req.body.email,
-//             address:req.body.address,
-//             password:hashedPassword,
-//         }).then(()=>{
-//             knex.select().from('student').then((student)=>{
-//                 res.send(student);
-//             });
-//         });
 
-//     }catch(error){
-//         res.status(500).json({error:error.message});
-//     }
-
-//     // knex.raw('insert into student (first_name,last_name) values(?,?)',['first','last'] ).then(()=>{
-//     //     knex.select().from('student').then((student)=>{
-//     //                 res.send(student);
-//     //             });
-//     // });
-// });
 
 router.put('/update/:id',(req,res) =>{
     const id=req.params.id;
@@ -130,12 +88,9 @@ router.get('/all',(req,res)=>{
     knex.select().from('student').then((student)=>{
         res.send(student);
     });
-    // knex.raw('select * from student').then((student)=>{
-    //     res.send(student.rows);
-    // });
+
 });
 
 
 
 module.exports=router;
-// export default router

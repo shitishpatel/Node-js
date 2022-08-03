b118e668ae181258be363d8d9202d43c98ebcbe5
shitishpatel <68268691+shitishpatel@users.noreply.github.com>
2022-08-03 15:14:36 +0530

    Update passportConfig.js

--- Project/passportConfig.js
+++ Project/passportConfig.js
@@ -3,16 +3,25 @@ const LocalStrategy=require('passport-local').Strategy;
 const {student} = require("./db/db");
 exports.initializingPassport = (passport)=>{
 
-    passport.use(new LocalStrategy(
-        function(username, password, done) {
-          student.findOne({ username: username }, function (err, user) {
-            if (err) { return done(err); }
-            if (!user) { return done(null, false); }
-            if (user.passwordn !==password) { return done(null, false); }
-            return done(null, user);
-          });
-        }
-      ));
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      student.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        bcrypt.compare(password,user.password,(err,match)=>{
+          if(err){
+            return done(null,false);
+          }
+          if(!match){
+            return done(null,false);
+          }
+          if(match){
+            return done(null,user);
+          }
+        })
+      });
+    }
+  ));
 
 
       passport.serializeUser((user,done)=>done(null,user.id));

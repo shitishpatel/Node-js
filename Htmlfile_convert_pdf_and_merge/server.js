a448a4ba45dea29c41b679563d8073f76b90f785
shitishpatel <68268691+shitishpatel@users.noreply.github.com>
2022-08-23 18:58:34 +0530

    Create server.js

---
+++ Htmlfile_convert_pdf_and_merge/server.js
@@ -0,0 +1,79 @@
+const {PDFDocument} = require('pdf-lib');
+const fs = require('fs');
+// const { runInContext } = require('vm');
+const pdf=require('html-pdf');
+
+const express = require('express');
+
+const app =express();
+
+const PORT=8000;
+
+app.post('/merge',(req,res)=>{
+    run().catch(err=>console.log(err));
+    res.send("merge");
+})
+
+
+app.get('/',(req,res)=>{
+    
+
+    var html=fs.readFileSync('./card1.html','utf-8');
+
+    let options={
+        format:'Letter'
+    }
+
+    pdf.create(html,options).toFile('./new1.pdf',function(err,res){
+        if(err) return console.log(err);
+        console.log(res);
+    })
+    res.send("<h1>Shitish</h1>");
+});
+
+app.post('/file',(req,res)=>{
+
+    var html=fs.readFileSync('./card2.html','utf-8');
+
+    let options={
+        format:'Letter'
+    }
+
+    pdf.create(html,options).toFile('./new2.pdf',function(err,res){
+        if(err) return console.log(err);
+        console.log(res);
+    })
+
+    res.send("rahul");
+})
+
+
+
+
+
+async function run(){
+    const cover= await PDFDocument.load(fs.readFileSync('./new1.pdf'));
+    const content= await PDFDocument.load(fs.readFileSync('./new2.pdf'));
+
+    const doc = await PDFDocument.create();
+
+    const contentPages1=await doc.copyPages(cover,cover.getPageIndices());
+    for(const page of contentPages1){
+        doc.addPage(page);
+    }
+
+    const contentPages2=await doc.copyPages(content,content.getPageIndices());
+    for(const page of contentPages2){
+        doc.addPage(page);
+    }
+
+    fs.writeFileSync('./test.pdf',await doc.save());
+}
+
+
+app.listen(PORT,()=>{
+    console.log(`listening to the port no ${PORT}`);
+});
+
+
+

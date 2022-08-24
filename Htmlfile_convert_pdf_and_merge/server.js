9b1f51d046f8c328ebce56510523674dab93c217
shitishpatel <68268691+shitishpatel@users.noreply.github.com>
2022-08-24 18:27:07 +0530

    Update server.js

--- Htmlfile_convert_pdf_and_merge/server.js
+++ Htmlfile_convert_pdf_and_merge/server.js
@@ -10,65 +10,109 @@ const app =express();
 const PORT=8000;
 
 app.post('/merge',(req,res)=>{
-    run().catch(err=>console.log(err));
-    res.send("merge");
-})
-
-
-app.get('/',(req,res)=>{
-    
-
-    var html=fs.readFileSync('./card1.html','utf-8');
+    convert();
+    async function convert(){
+        var html= fs.readFileSync('./card2.html','utf-8');
 
     let options={
         format:'Letter'
     }
 
-    pdf.create(html,options).toFile('./new1.pdf',function(err,res){
+    pdf.create(html,options).toFile('./new2.pdf', function(err,res){
         if(err) return console.log(err);
         console.log(res);
     })
-    res.send("<h1>Shitish</h1>");
-});
 
-app.post('/file',(req,res)=>{
+    var html1= fs.readFileSync('./card1.html','utf-8');
 
-    var html=fs.readFileSync('./card2.html','utf-8');
+   
 
-    let options={
-        format:'Letter'
-    }
-
-    pdf.create(html,options).toFile('./new2.pdf',function(err,res){
+    pdf.create(html1,options).toFile('./new1.pdf', function(err,res){
         if(err) return console.log(err);
         console.log(res);
     })
 
     res.send("rahul");
+    }
+     merge().catch(err=>console.log(err));
+    // res.send("merge");
+
+    async function merge(){
+        const cover= await PDFDocument.load(fs.readFileSync('./new1.pdf'));
+        const content= await PDFDocument.load(fs.readFileSync('./new2.pdf'));
+    
+        const doc = await PDFDocument.create();
+    
+        const contentPages1=await doc.copyPages(cover,cover.getPageIndices());
+        for(const page of contentPages1){
+            doc.addPage(page);
+        }
+    
+        const contentPages2=await doc.copyPages(content,content.getPageIndices());
+        for(const page of contentPages2){
+            doc.addPage(page);
+        }
+    
+        fs.writeFileSync('./test.pdf',await doc.save());
+    }
+
 })
 
 
+// app.get('/',(req,res)=>{
+    
+
+//     var html=fs.readFileSync('./card1.html','utf-8');
+
+//     let options={
+//         format:'Letter'
+//     }
+
+//     pdf.create(html,options).toFile('./new1.pdf',function(err,res){
+//         if(err) return console.log(err);
+//         console.log(res);
+//     })
+//     res.send("<h1>Shitish</h1>");
+// });
+
+// app.post('/file',(req,res)=>{
+
+//     var html=fs.readFileSync('./card2.html','utf-8');
+
+//     let options={
+//         format:'Letter'
+//     }
+
+//     pdf.create(html,options).toFile('./new2.pdf',function(err,res){
+//         if(err) return console.log(err);
+//         console.log(res);
+//     })
+
+//     res.send("rahul");
+// })
 
 
 
-async function run(){
-    const cover= await PDFDocument.load(fs.readFileSync('./new1.pdf'));
-    const content= await PDFDocument.load(fs.readFileSync('./new2.pdf'));
 
-    const doc = await PDFDocument.create();
 
-    const contentPages1=await doc.copyPages(cover,cover.getPageIndices());
-    for(const page of contentPages1){
-        doc.addPage(page);
-    }
+// async function run(){
+//     const cover= await PDFDocument.load(fs.readFileSync('./new1.pdf'));
+//     const content= await PDFDocument.load(fs.readFileSync('./new2.pdf'));
 
-    const contentPages2=await doc.copyPages(content,content.getPageIndices());
-    for(const page of contentPages2){
-        doc.addPage(page);
-    }
+//     const doc = await PDFDocument.create();
 
-    fs.writeFileSync('./test.pdf',await doc.save());
-}
+//     const contentPages1=await doc.copyPages(cover,cover.getPageIndices());
+//     for(const page of contentPages1){
+//         doc.addPage(page);
+//     }
+
+//     const contentPages2=await doc.copyPages(content,content.getPageIndices());
+//     for(const page of contentPages2){
+//         doc.addPage(page);
+//     }
+
+//     fs.writeFileSync('./test.pdf',await doc.save());
+// }
 
 
 app.listen(PORT,()=>{
